651dea910340ece7c247c109f17d786cde5ed7d0
shaymurat <shaymuratoff@ya.ru>
2024-08-03 00:52:22 +0500

    Add files via upload

--- module_3_2.py
+++ module_3_2.py
@@ -4,16 +4,16 @@ def send_email(message, recipient, *, sender='university.help@gmail.com'):
             or '@' not in sender
             or not recipient.endswith(suffixes)
             or not sender.endswith(suffixes)):
-        print(f'Невозможно отправить письмо с адреса <{sender}> на'
-              f' адрес <{recipient}>')
+        print(f'Невозможно отправить письмо с адреса {sender} на'
+              f' адрес {recipient}')
     elif recipient == sender:
         print('Нельзя отправить письмо самому себе!')
     elif sender == 'university.help@gmail.com':
-        print(f'Письмо успешно отправлено с адреса <{sender}> на'
-              f' адрес <{recipient}>.')
+        print(f'Письмо успешно отправлено с адреса {sender} на'
+              f' адрес {recipient}.')
     else:
         print('НЕСТАНДАРТНЫЙ ОТПРАВИТЕЛЬ! Письмо отправлено с адреса'
-              f' <{sender}> на адрес <{recipient}>.')
+              f' {sender} на адрес {recipient}.')
 
 
 send_email('Это сообщение для проверки связи', 'vasyok1337@gmail.com')
